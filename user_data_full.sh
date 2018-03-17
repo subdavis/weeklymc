10d472aa26438c1a83a20e1a4d7b1efe16015605
subdavis <brandonadavis@outlook.com>
2018-03-16 21:52:26 -0400

    run at every boot

--- user_data_full.sh
+++ user_data_full.sh
@@ -4,6 +4,10 @@
 source /etc/environment
 source $SCRIPTDIR/vars.sh
 
+cd $SCRIPTDIR
+git pull  # Runs at every reboot, so there could have been changes.
+
+# These directories are needed later.
 mkdir -p $APPDIR
 mkdir -p $LOGDIR
 
@@ -27,18 +31,22 @@ fi
 # Get server jar from S3
 aws s3 cp s3://$S3BUCKET/jars/spigot.jar $APPDIR/spigot.jar
 
+# Schedule CRON startup and shutdown.
+crontab -l > currentcron
+
 # IF we should start the server immediately on boot
 if [ "$AUTOSTART" == "true" ]; then
 	$SCRIPTDIR/session.sh begin
 	sleep 86400 # In case we need to keep docker alive
 else
-	# Schedule CRON startup and shutdown.
-	crontab -l > currentcron
 	#echo new cron into cron file
 	echo "$STARTCRON   $SCRIPTDIR/session.sh begin" >> newcron
 	echo "$STOPCRON    $SCRIPTDIR/session.sh end"   >> newcron
 	echo "$NOTIFY_TIME $SCRIPTDIR/session.sh notify players \"$NOTIFY_MESSAGE\"" >> newcron
-	#install new cron file
-	crontab newcron
-	rm newcron
 fi
+
+# Always run this script at reboot.
+echo "@reboot      $SCRIPTDIR/user_data_full.sh" >> newcron
+#install new cron file
+crontab newcron
+rm newcron
